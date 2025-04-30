e57fa19dcef6fa9d2f3631f6ec82fcc3521e3e36
Anthony Ettinger <anthony@chovy.com>
2025-04-30 01:45:52 -0700

    Update spa-router dependency to version 1.8.2

--- public/js/deps.js
+++ public/js/deps.js
@@ -7,7 +7,7 @@
  */
 
 // Import and re-export spa-router
-export { Router, transitions, renderer, componentLoader } from 'https://esm.sh/@profullstack/spa-router@1.8.1';
+export { Router, transitions, renderer, componentLoader } from 'https://esm.sh/@profullstack/spa-router@1.8.2';
 
 // Import and re-export state-manager
 export { createStore, StoreConnector } from 'https://esm.sh/@profullstack/state-manager@1.0.1';
