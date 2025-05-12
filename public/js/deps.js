4e5c1470ea6833af10f35bfe2649f4b4f7284b70
Anthony Ettinger <anthony@chovy.com>
2025-05-12 02:02:30 -0700

    chore: upgrade state-manager package to v0.5.18 and simplify imports

--- public/js/deps.js
+++ public/js/deps.js
@@ -19,7 +19,6 @@ export * as enhancedRouter from 'https://esm.sh/@profullstack/enhanced-router@0.
 export * as apiKeyManager from 'https://esm.sh/@profullstack/api-key-manager@0.2.13';
 export * as authSystem from 'https://esm.sh/@profullstack/auth-system@0.2.16';
 export * as paymentGateway from 'https://esm.sh/@profullstack/payment-gateway@0.2.16';
-export * as stateManager from 'https://esm.sh/@profullstack/state-manager@0.5.17';
+export * as stateManager from 'https://esm.sh/@profullstack/state-manager@0.5.18';
 export * as storageService from 'https://esm.sh/@profullstack/storage-service@0.2.15';
-export { StoreConnector } from 'https://esm.sh/@profullstack/state-manager@0.5.17';
-export { createStateManager as createStore } from 'https://esm.sh/@profullstack/state-manager@0.5.17';
+export { StoreConnector, createStore } from 'https://esm.sh/@profullstack/state-manager@0.5.18';
