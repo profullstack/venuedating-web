1f557abf0631453b8a409ac2bea4ea7767d86f87
Anthony Ettinger <anthony@chovy.com>
2025-05-12 01:40:55 -0700

    chore: update profullstack dependency versions and remove document-converters module

--- public/js/deps.js
+++ public/js/deps.js
@@ -7,18 +7,17 @@
  */
 
 // Import and re-export spa-router
-export { Router, transitions, renderer, componentLoader } from 'https://esm.sh/@profullstack/spa-router@1.12.12';
+export { Router, transitions, renderer, componentLoader } from 'https://esm.sh/@profullstack/spa-router@1.12.13';
 
 // Import and re-export localizer
-export { localizer, _t } from 'https://esm.sh/@profullstack/localizer@0.6.14';
+export { localizer, _t } from 'https://esm.sh/@profullstack/localizer@0.6.15';
 
 // Import and re-export enhanced-router
-export * as enhancedRouter from 'https://esm.sh/@profullstack/enhanced-router@0.2.14';
+export * as enhancedRouter from 'https://esm.sh/@profullstack/enhanced-router@0.2.15';
 
 // Re-export other modules
-export * as apiKeyManager from 'https://esm.sh/@profullstack/api-key-manager@0.2.12';
-export * as authSystem from 'https://esm.sh/@profullstack/auth-system@0.2.15';
-export * as documentConverters from 'https://esm.sh/@profullstack/document-converters@0.2.15';
-export * as paymentGateway from 'https://esm.sh/@profullstack/payment-gateway@0.2.15';
-export * as storageService from 'https://esm.sh/@profullstack/storage-service@0.2.14';
-export * as websocketClient from 'https://esm.sh/@profullstack/websocket-client@0.2.15';
+export * as apiKeyManager from 'https://esm.sh/@profullstack/api-key-manager@0.2.13';
+export * as authSystem from 'https://esm.sh/@profullstack/auth-system@0.2.16';
+export * as paymentGateway from 'https://esm.sh/@profullstack/payment-gateway@0.2.16';
+export * as storageService from 'https://esm.sh/@profullstack/storage-service@0.2.15';
+export * as websocketClient from 'https://esm.sh/@profullstack/websocket-client@0.2.16';
